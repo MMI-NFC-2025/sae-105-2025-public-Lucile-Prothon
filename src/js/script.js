b2a27d00cfeade26e93d346781c8617701ff7b81
Lucile-Prothon <lucile.prothon@edu.univ-fcomte.fr>
2025-12-11 10:37:15 +0100

    Modif header + changement icone menu + ajout police menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,12 @@
 /* Menu*/
 const toggle = document.querySelector('.menu-btn');
 const nav = document.querySelector(".menu");
+
 toggle.addEventListener('click', () => {
-    const isOpen = toggle.ariaExpanded === "true";
-    const isClosed = !isOpen;
-    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
-    nav.ariaHidden = isOpen;
-    toggle.ariaExpanded = isClosed;
-    document.body.classList.toggle("noscroll");
+    const isOpen = toggle.getAttribute("aria-expanded") === "true";
+
+    toggle.setAttribute("aria-expanded", !isOpen);
+    nav.setAttribute("aria-hidden", isOpen);
+
+    document.body.classList.toggle("noscroll", !isOpen);
 });
